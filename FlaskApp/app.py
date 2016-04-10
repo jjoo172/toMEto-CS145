fb3e164f3241b9c13b4d56f90a7b0172e5d16371
jonathan <joo@caltech.edu>
2016-04-09 20:56:46 -0700

    can get some data working

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+	print "eieie"
 	return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
-    return render_template('signup.html')
+	print "asdfasdf"
+	return render_template('signup.html')
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
@@ -19,11 +21,16 @@ def signUp():
  
     # validate the received values
     if _name and _email and _password:
+    	print "tite"
         return json.dumps({'html':'<span>All fields good !!</span>'})
+
     else:
+    	print "not all things filled in"
         return json.dumps({'html':'<span>Enter the required fields</span>'})
+      
 
 if __name__ == "__main__":
+	print "aaaaa"
 	app.run()
 
 
