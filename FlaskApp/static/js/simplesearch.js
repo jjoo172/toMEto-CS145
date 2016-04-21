f622a44aa16a52483f49cbdeebc96467490be531
jonathan <joo@caltech.edu>
2016-04-21 01:57:16 -0700

    fixed formatting bug

---
+++ FlaskApp/static/js/simplesearch.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#btnSearch').click(function() {
+        $.ajax({
+            url: '/searchQuery',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
