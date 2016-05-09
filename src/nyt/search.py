9a2d5767de5cc7307d20bf05208df08e920f362d
tongcharlie <tongcharlie@gmail.com>
2016-05-08 18:44:57 -0700

    redo/fix search bug

--- src/nyt/search.py
+++ src/nyt/search.py
@@ -1,5 +1,5 @@
 import urllib2
-from bs4 import BeautifulSoup
+import re
 
 
 def search(query):
@@ -8,19 +8,7 @@ def search(query):
 
     req = urllib2.urlopen(url)
     content = req.read()
+    
+    pattern = r'data-url="/recipes/(\d+)'
 
-    soup = BeautifulSoup(content, 'html.parser')
-
-    articles_result = soup.find_all('article')
-
-    result_ids = []
-
-    for a in articles_result:
-        try:
-            result_ids.append(a["data-id"])
-        except Exception as e:
-            print "Error retrieving some article"
-            # this is typically because of a paid post advertisement...
-
-
-    return result_ids
+    return re.findall(pattern, content)
